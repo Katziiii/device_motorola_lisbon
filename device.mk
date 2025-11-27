7eafbb022e494806a4671597fa71b3bf38964179
HollowAOSP <ahmedshahinx0x@gmail.com>
2025-11-27 13:31:50 -0500

    lisbon: Drop unused init configs
    Change-Id: If6d76a835ede0760cfc71aad131e84fac1ffb408

--- device.mk
+++ device.mk
@@ -322,52 +322,40 @@ PRODUCT_PACKAGES += \
 
 # Rootdir
 PRODUCT_PACKAGES += \
-    pstore_annotate.sh \
-    hardware_revisions.sh \
-    init.mmi.usb.sh \
-    init.mmi.backup.trustlet.sh \
     apanic_annotate.sh \
-    init.oem.fingerprint.overlay.sh \
-    init.mmi.block_perm.sh \
-    init.oem.fingerprint2.overlay.sh \
-    init.insmod.sh \
     apanic_copy.sh \
+    apanic_mtk.sh \
     apanic_save.sh \
+    hardware_revisions.sh \
+    init.insmod.sh \
+    init.mmi.backup.trustlet.sh \
+    init.mmi.block_perm.sh \
     init.mmi.boot.sh \
     init.mmi.modules.sh \
-    init.oem.hw.sh \
-    apanic_mtk.sh \
     init.mmi.touch.sh \
+    init.mmi.usb.sh \
+    init.oem.fingerprint.overlay.sh \
+    init.oem.fingerprint2.overlay.sh \
+    init.oem.hw.sh \
+    pstore_annotate.sh \
 
 PRODUCT_PACKAGES += \
     fstab.mt6785 \
-    meta_init.connectivity.rc \
-    init.project.rc \
-    meta_init.rc \
-    factory_init.rc \
-    init.connectivity.common.rc \
-    multi_init.rc \
-    init.modem.rc \
-    init.sensor_1_0.rc \
-    init.mmi.chipset.rc \
-    apanic.rc \
-    init.mmi.rc \
     init.ago.rc \
-    init.mt6785.rc \
-    meta_init.project.rc \
-    init.mmi.tcmd.rc \
-    factory_init.connectivity.rc \
-    init.mmi.overlay.rc \
+    init.connectivity.common.rc \
     init.connectivity.rc \
-    init_connectivity.rc \
-    factory_init.connectivity.common.rc \
-    meta_init.modem.rc \
-    meta_init.connectivity.common.rc \
+    init.modem.rc \
     init.mmi.backup.trustlet.rc \
-    factory_init.project.rc \
+    init.mmi.chipset.rc \
+    init.mmi.overlay.rc \
+    init.mmi.rc \
+    init.mt6785.rc \
     init.mt6785.usb.rc \
-    init.aee.rc \
     init.recovery.mt6785.rc \
+    init.project.rc \
+    init.sensor_1_0.rc \
+    init_connectivity.rc \
+    ueventd.mt6785.rc
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.mt6785:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6785
